01e2a8c9c8964e926e0d77d8ffec7a86d8274aef
lem325 <107568796+lem325@users.noreply.github.com>
2023-10-06 22:10:13 -0400

    for testing change

--- thrive_for_girls/App.js
+++ thrive_for_girls/App.js
@@ -2,6 +2,10 @@ import { StyleSheet, View } from "react-native";
 import Logo from "./components/Logo";
 import Welcome from "./components/Welcome";
 import Input from "./components/Input";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
@@ -12,6 +16,14 @@ export default function App() {
     </View>
   );
 }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <StackNavigator/>
+//     </NavigationContainer>
+//   );
+// }
 
 const styles = StyleSheet.create({
 //   container: {
