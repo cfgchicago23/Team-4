ff8c7fdb3ab395867bab5302e081892108e26a6b
kmeichao <kayleemeichao@gmail.com>
2023-10-06 22:17:40 -0500

    Login button and fixed login nav

--- thrive_for_girls/StackNavigator.js
+++ thrive_for_girls/StackNavigator.js
@@ -1,15 +1,15 @@
 import { View, Text, BackHandler } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginTemp from "./src/screens/LoginTemp";
+import Login from "./src/screens/Login";
 import TabNavigator from "./TabNavigator";
 
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <Stack.Navigator >
-        <Stack.Screen name='LoginTemp' component={LoginTemp}/>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='LoginTemp' component={Login}/>
         <Stack.Screen name='HomeScreen' component={TabNavigator}/>
     </Stack.Navigator>
   );
