d9fe435da63906aefe3c30a17ed6e4fb77669cf8
c00lme <ksrivatsa19@gmail.com>
2023-10-06 21:14:09 -0700

    Made styling adjustments to the forum page in a list view, will include routing and navigation to comments

--- thrive_for_girls/src/screens/Forum.js
+++ thrive_for_girls/src/screens/Forum.js
@@ -1,23 +1,28 @@
-// components/PostList.js
-import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+// Forum.js
 
-const PostList = ({ posts, navigation }) => {
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { posts } from './ForumData'; // Import your sample data
+
+const Forum = ({ navigation }) => {
   return (
-    <FlatList
-      data={posts}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <TouchableOpacity
-          onPress={() => navigation.navigate('PostDetail', { post: item })}
-        >
-          <View>
-            <Text>{item.title}</Text>
-          </View>
-        </TouchableOpacity>
-      )}
-    />
+    <View style={{ flex: 1, padding: 16 }}>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('PostDetails', { post: item })}
+          >
+            <View style={{ padding: 16, borderBottomWidth: 1, borderColor: '#ccc' }}>
+              <Text style={{ fontSize: 18 }}>{item.title}</Text>
+              <Text style={{ marginTop: 8 }}>{item.content}</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
   );
 };
 
-export default PostList;
+export default Forum;
---
+++ thrive_for_girls/src/screens/PostDetails.js
@@ -0,0 +1,32 @@
+// PostDetails.js
+
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+const PostDetails = ({ route }) => {
+  const { post } = route.params;
+
+  // Assuming you have a comments array in your mockData.js
+  const commentsForPost = comments.filter((comment) => comment.postId === post.id);
+
+  return (
+    <View style={{ flex: 1, padding: 16 }}>
+      <View style={{ padding: 16, borderBottomWidth: 1, borderColor: '#ccc' }}>
+        <Text style={{ fontSize: 18 }}>{post.title}</Text>
+        <Text style={{ marginTop: 8 }}>{post.content}</Text>
+      </View>
+      <Text style={{ marginTop: 16, fontSize: 20 }}>Comments:</Text>
+      <FlatList
+        data={commentsForPost}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={{ padding: 16, borderBottomWidth: 1, borderColor: '#ccc' }}>
+            <Text>{item.text}</Text>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default PostDetails;
