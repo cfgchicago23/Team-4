786bdf423c7368a063b1004856c96b712084997d
Elida <elidavillehas03@icloud.com>
2023-10-07 05:00:21 -0500

    Commit Message

--- thrive_for_girls/src/screens/Lessons.js
+++ thrive_for_girls/src/screens/Lessons.js
@@ -1,10 +1,115 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { StyleSheet, FlatList, TouchableOpacity, View, Image, Text, StatusBar } from 'react-native';
 
-export default function Lessons() {
+const Lessons = () => {
+  const data = [
+    {
+      id: 1,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson One',
+      color: '#11ad09',
+      completed: 1,
+    },
+
+    {
+      id: 2,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson Two',
+      color: '#11ad09',
+      completed: 1,
+    },
+
+    {
+      id: 3,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson Three',
+      color: '#11ad09',
+      completed: 1,
+    },
+
+    {
+      id: 4,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson Four',
+      color: '#11ad09',
+      completed: 1,
+    },
+
+    {
+      id: 5,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson Five',
+      color: '#11ad09',
+      completed: 1,
+    },
+
+    {
+      id: 6,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson Six',
+      color: '#11ad09',
+      completed: 1,
+    },
+
+    {
+      id: 7,
+      image: 'https://i.pinimg.com/564x/f1/5f/4d/f15f4df7c5c1a3d939f46719d2c2b55c.jpg',
+      description: 'Lesson Seven',
+      color: '#11ad09',
+      completed: 1,
+    },
+  ];
+
+  const [lessons, setLessons] = useState(data);
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.lesson} onPress={() => {}}>
+      <Image style={styles.image} source={{ uri: item.image }} />
+      <View style={styles.details}>
+        <Text style={styles.description}>{item.description}</Text>
+        <Text style={styles.id}>{item.id}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
-    <View>
-      <Text>Lessons</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={lessons}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight || 0,
+  },
+  lesson: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    marginRight: 10,
+  },
+  details: {
+    flex: 1,
+  },
+  description: {
+    fontSize: 18,
+  },
+  id: {
+    fontSize: 14,
+    color: '#666',
+  },
+});
+
+export default Lessons;
